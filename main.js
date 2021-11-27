94d816ef8f4d615d6dae0d930778e0f2869ff410
Xavier Garcia <45861163+xdg6227@users.noreply.github.com>
2021-11-27 11:50:13 -0700

    Edited Procfile

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-const { Client, Intents } = require('discord.js');
+const { Client, Intents, Collection } = require('discord.js');
+const { readdirSync } = require('fs');
 
 global.client = new Client({
   intents: [
@@ -8,7 +9,21 @@ global.client = new Client({
 });
 
 client.config = require('./config');
+client.commands = new Collection();
 
-require('./src/loader');
+const events = readdirSync('./events/').filter(file => file.endsWith('.js'));
+const commands = readdirSync('./commands/').filter(file => file.endsWith('.js'));
+
+for (const file of events) {
+  const event = require(`../events/${file}`);
+  client.on(file.split('.')[0], event.bind(null, client));
+  delete require.cache[require.resolve(`../events/${file}`)];
+};
+
+for (const file of commands) {
+  const command = require(`../commands/${file}`);
+  client.commands.set(command.name.toLowerCase(), command);
+  delete require.cache[require.resolve(`../commands/${file}`)];
+};
 
 client.login(client.config.dsc.token);
