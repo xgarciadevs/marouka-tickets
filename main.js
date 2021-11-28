1b522ee1b5f9275bb5de620790482c58d1c916a7
Xavier Garcia <45861163+xdg6227@users.noreply.github.com>
2021-11-28 14:37:31 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const { Client, Intents, Collection } = require('discord.js');
-const { readdirSync } = require('fs');
+const fs = require('fs');
 
 global.client = new Client({
   intents: [
@@ -11,19 +11,18 @@ global.client = new Client({
 client.config = require('./config');
 client.commands = new Collection();
 
-const events = readdirSync('./events/').filter(file => file.endsWith('.js'));
-const commands = readdirSync('./commands/').filter(file => file.endsWith('.js'));
+fs.readdirSync('./commands').forEach(file => {
+  const command = require(`./commands/${file}`);
+  client.commands.set(command.name, command);
+  if (!command.aliases) return;
+  command.aliases.forEach((alias) => {
+    client.aliases.set(alias, command);
+  });
+});
 
-for (const file of events) {
-  const event = require(`../events/${file}`);
+fs.readdirSync('./events').forEach(file => {
+  const event = require(`./events/${file}`);
   client.on(file.split('.')[0], event.bind(null, client));
-  delete require.cache[require.resolve(`../events/${file}`)];
-};
-
-for (const file of commands) {
-  const command = require(`../commands/${file}`);
-  client.commands.set(command.name.toLowerCase(), command);
-  delete require.cache[require.resolve(`../commands/${file}`)];
-};
+});
 
 client.login(client.config.dsc.token);
